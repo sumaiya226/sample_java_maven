23a25654cd09084c6717d5fc631f7e1df1d6ba83
Sumaiya Shaik <sumaiya.shaik@example.com>
2024-11-16 15:02:26 +0530

    Initial commit of Maven Java project

---
+++ src/main/java/sample_java_maven/sample_j_maven/App.java
@@ -0,0 +1,7 @@
+package sample_java_maven.sample_j_maven;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/sample_java_maven/sample_j_maven/AppTest.java
@@ -0,0 +1,13 @@
+package sample_java_maven.sample_j_maven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
